88d4ce1ddebbe93cbc1ba951cb9b1b5b6d282618
Iris Ward <dan@wardy.org>
2015-08-23 02:22:03 +0100

    Move WaniKani API Client to submodule

--- WaniKaniClient/Utils/Extensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace WaniKaniClientLib.Utils
-{
-    internal static class Utilities
-    {
-        public static DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-    }
-}
