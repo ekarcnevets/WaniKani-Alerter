604f44e23ca756676c3e72a20897efcb8a433a5c
Iris Ward <dan@wardy.org>
2015-08-23 18:18:48 +0100

    Update meta-information on Alerter
    Change the name and company given; update assembly information;
    fix the application icon.

--- WaniKaniAlerter/Properties/AssemblyInfo.cs
+++ WaniKaniAlerter/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -8,9 +9,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("WaniKaniAlerter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("AdituV")]
 [assembly: AssemblyProduct("WaniKaniAlerter")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Iris Ward  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,3 +34,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.9.*")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
